d5de416eeb8ce044c1e08092574ea15693afca81
Rashi Aggarwal <146416686+rashiaggarwal06@users.noreply.github.com>
2025-07-02 14:55:06 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (31.57%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
@@ -11,15 +11,36 @@
  */
 class Solution {
 public:
-    void inorder(TreeNode* root, vector<int>& res) {
-        if (!root) return;
-        inorder(root->left, res);
-        res.push_back(root->val);
-        inorder(root->right, res);
-    }
+    // void inorder(TreeNode* root, vector<int>& res) {
+    //     if(!root) return;
+    //     inorder(root->left, res);
+    //     res.push_back(root->val);
+    //     inorder(root->right, res);
+    // }
+    // vector<int> inorderTraversal(TreeNode* root) {
+    //     vector<int> res;
+    //     inorder(root, res);
+    //     return res;
+    // }
+
     vector<int> inorderTraversal(TreeNode* root) {
         vector<int> res;
-        inorder(root, res);
+        stack<TreeNode*> st;
+        TreeNode* curr = root;
+        while(curr != NULL || !st.empty()){
+            //Reach the leftmost node of the current subtree
+            while (curr != NULL) {
+                st.push(curr);
+                curr = curr->left;
+            }
+            // curr is NULL at this point
+            curr = st.top();
+            st.pop();
+            // Visit the node
+            res.push_back(curr->val);
+            // Move to right subtree
+            curr = curr->right;
+        }
         return res;
     }
 };
