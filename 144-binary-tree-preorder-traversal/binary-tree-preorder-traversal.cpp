3cd1b2539f1c04c51fccca3baa490a179d68dabe
Rashi Aggarwal <146416686+rashiaggarwal06@users.noreply.github.com>
2025-07-02 14:47:53 +0530

    Time: 3 ms (2.85%) | Memory: 11 MB (14.71%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
@@ -11,19 +11,37 @@
  */
 class Solution {
 public:
-    void preorder(TreeNode* root, vector<int>& res){
-        //Base Case:
-        if(root == NULL) return;
-        //Fetch the root value
-        res.push_back(root->val);
-        //Left values of tree
-        preorder(root->left, res);
-        //Right values of tree
-        preorder(root->right, res);
-    }
-    vector<int> preorderTraversal(TreeNode* root) {
+    // void preorder(TreeNode* root, vector<int>& res){
+    //     //Base Case:
+    //     if(root == NULL) return;
+    //     //Fetch the root value
+    //     res.push_back(root->val);
+    //     //Left values of tree
+    //     preorder(root->left, res);
+    //     //Right values of tree
+    //     preorder(root->right, res);
+    // }
+    // vector<int> preorderTraversal(TreeNode* root) {
+    //     vector<int> res;
+    //     preorder(root, res);
+    //     return res;
+    // }
+    vector<int> preorderTraversal(TreeNode* root){
         vector<int> res;
-        preorder(root, res);
+        if(root == NULL) return res;
+        stack<TreeNode*> st;
+        st.push(root);
+        while(!st.empty()){
+            TreeNode* curr = st.top();
+            st.pop();
+            // Visit the current node
+            res.push_back(curr->val);
+            // Push right child first so left is processed first
+            if(curr->right)
+                st.push(curr->right);
+            if(curr->left)
+                st.push(curr->left);
+        }
         return res;
     }
 };
