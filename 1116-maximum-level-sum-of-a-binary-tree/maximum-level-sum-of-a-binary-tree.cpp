260699b0c8d3784bb3019bc6518ed8d9adfd056f
Rashi Aggarwal <146416686+rashiaggarwal06@users.noreply.github.com>
2025-07-02 20:09:58 +0530

    Time: 14 ms (5.74%) | Memory: 112.1 MB (10.41%) - LeetSync

---
+++ 1116-maximum-level-sum-of-a-binary-tree/maximum-level-sum-of-a-binary-tree.cpp
@@ -0,0 +1,60 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void lvlOrder(TreeNode* root, vector<int>& level, vector<vector<int>>& res){
+        //If root is empty
+        if(root == NULL) return;
+        //Make a queue to push every element for each level
+        queue<TreeNode*> q; 
+        //Push the root element first in the queue
+        q.push(root); 
+        //Iterate in the tree while the queue does not get empty
+        while(q.size() > 0){
+            //Find the size of the current level
+            int k = q.size();  
+            //Clear the level
+            level.clear();
+            //iterate till the current level exists          
+            for(int i = 0; i < k; i++){
+                //Store the front value in a node and pop it out
+                TreeNode* curr = q.front();
+                q.pop();
+                //push that value in the level vector we have created which is sub part of the resultant vector
+                level.push_back(curr->val); 
+                //Check further if the current nodes left exists, then push it in queue and same for the current nodes right 
+                if(curr->left != NULL)
+                    q.push(curr->left);
+                if(curr->right != NULL)
+                    q.push(curr->right);
+            }
+            //Push the whole current level to the result
+            res.push_back(level); 
+        }
+    }
+    int maxLevelSum(TreeNode* root){
+        vector<vector<int>> res;
+        vector<int> level;
+        lvlOrder(root, level, res);
+        int maxLevel = 0, maxSum = INT_MIN;
+        for(int i = 0; i < res.size(); i++){
+            int s = 0;
+            for(int x : res[i])
+                s += x;
+            if(s > maxSum){
+                maxSum = s;
+                maxLevel = i + 1;
+            }
+        }
+        return maxLevel;
+    }
+};
