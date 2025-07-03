feae402356f42d00aa71cad9d90558da84d11cf3
Rashi Aggarwal <146416686+rashiaggarwal06@users.noreply.github.com>
2025-07-03 10:17:19 +0530

    Time: 1 ms (43.20%) | Memory: 28.9 MB (70.20%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.cpp
+++ 572-subtree-of-another-tree/subtree-of-another-tree.cpp
@@ -11,17 +11,25 @@
  */
 class Solution {
 public:
+    //Function to check if all the further nodes of subtree are present in tree as it is or not
     bool isSame(TreeNode* root, TreeNode* subroot){
+        //If both values are null means both are identical
         if(root == NULL && subroot == NULL) return true;
+        //if any of the value is null, means not identical
         if(root == NULL || subroot == NULL) return false;
+        //If values of nodes of tree and subtree are not equal
         if(root->val != subroot->val) return false;
+        //Continue checking for next values below in the tree for left and right both
         return isSame(root->left, subroot->left) && isSame(root->right, subroot->right);  
     }
+
     bool isSubtree(TreeNode* root, TreeNode* subroot){
+        //If root is null or subroot is null, return they are equal if really equals it will return true else false
         if(root == NULL || subroot == NULL) return root == subroot;
-        if(root->val == subroot->val && isSame(root, subroot)){
+        //Check the values for root and subroot and also check if the trees are same or not. If trees are same means yes subroot is subtree of the given tree
+        if(root->val == subroot->val && isSame(root, subroot))
             return true;
-        }
+        //Continue checking for root's left and root's right until we hit the base cases
         return isSubtree(root->left, subroot) || isSubtree(root->right, subroot);
     }
 };
